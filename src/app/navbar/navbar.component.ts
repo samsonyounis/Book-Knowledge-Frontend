3308a912dc44cb034d52268179bc481c8e9fd949
samsonyounis <samsonyounis26@gmail.com>
2025-02-02 20:17:35 +0300

    addedd node v5

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { UserServiceService } from '../user-service.service';
 import { RouterModule } from '@angular/router';
 
@@ -14,13 +14,35 @@ export class NavbarComponent {
  public users:object = [];
 
  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
-
+ isNavbarOpen = false;
 
  constructor(private userservice: UserServiceService){}
 
  closeNavbar() {
   if (this.navbarCollapse) {
     this.navbarCollapse.nativeElement.classList.remove('show'); // Collapses menu
+  }
+  // this.isNavbarOpen = false;
+  // this.navbarCollapse.nativeElement.classList.remove('show');
+}
+toggleNavbar() {
+  this.isNavbarOpen = !this.isNavbarOpen;
+  if (this.isNavbarOpen) {
+    this.navbarCollapse.nativeElement.classList.add('show');
+  } else {
+    this.navbarCollapse.nativeElement.classList.remove('show');
+  }
+}
+
+// Close Navbar When Clicking Outside
+@HostListener('document:click', ['$event'])
+onClickOutside(event: Event) {
+  if (
+    this.isNavbarOpen &&
+    this.navbarCollapse &&
+    !this.navbarCollapse.nativeElement.contains(event.target)
+  ) {
+    this.closeNavbar();
   }
 }
 
