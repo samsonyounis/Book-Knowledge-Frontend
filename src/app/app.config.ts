43eb7d944edfff1758d13468409036a8c13298cc
samsonyounis <samsonyounis26@gmail.com>
2025-04-05 22:46:23 +0300

    some commits

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
 import { routes } from './app.routes';
 import { errorInterceptor } from './errorinterceptor.service';
+import {withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor])),DatePipe,
-    provideRouter(routes) // Provide the router with routes
+    provideRouter(routes, withHashLocation()) // Provide the router with routes
   ]
 };
